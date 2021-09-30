5be67c671f0a0ec7de7f4b032687fe8ffb8e4d72
Marcus Allen <emkay1993@gmail.com>
2021-09-30 10:54:05 +0100

    Exception notification setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
                                           email: {
                                             deliver_with: :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
                                             email_prefix: '[PREFIX] ',
-                                            sender_address: %("Edfu error" <support@edfu.herokuapp.com>),
+                                            sender_address: %("Edfu error" <edfuhelper@gmail.com>),
                                             exception_recipients: %w[emkay1993@gmail.com]
                                           }
   # Settings specified here will take precedence over those in config/application.rb.
