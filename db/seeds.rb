a53e38721cea49fbc7b85b8f814770f3137410ed
Marcus Allen <emkay1993@gmail.com>
2021-09-30 14:04:54 +0100

    Styling change for course show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-User.create({ email: 'emkay1993@gmail.com', password: 'pass1234', password_confirmation: 'pass1234' })
+# User.create({ email: 'emkay1993@gmail.com', password: 'pass1234', password_confirmation: 'pass1234' })
 
 lang_array = %w[English Chinese Russian Spanish French]
+
+user = User.new(
+  email: 'admin@example.com',
+  password: 'pass1234',
+  password_confirmation: 'pass1234'
+)
+
+user.skip_confirmation!
+user.save!
 
 30.times do
   Course.create([{
